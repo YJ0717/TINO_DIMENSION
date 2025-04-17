b1b16760e5a7ed25122d3856cccabf75065cdf42
YDH-12 <lalen1207@naver.com>
2025-04-18 00:28:33 +0900

    파일복구1

---
+++ TinoDimesionVanguard/Source/TinoDimesionVanguard/Public/server_connect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "server_connect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINODIMESIONVANGUARD_API Userver_connect : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
